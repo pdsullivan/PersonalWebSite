e91809f2c73f424157ed1a6a8cc534c9338ad74a
Patrick Sullivan <patdsully88@gmail.com>
2014-03-19 08:08:46 -0500

    Added tostring overide

--- PatrickSullivanWebSite/Models/ContactInfo.cs
+++ PatrickSullivanWebSite/Models/ContactInfo.cs
@@ -11,4 +11,9 @@ namespace PatrickSullivanWebSite.Models
         public string Email { get; set; }
         public string Message { get; set; }
     }
+    
+    protected override ToString()
+    {
+        return "ContactInfo: name = " + Name + " email = " + Email + " message = " + Message;
+    }
 }
