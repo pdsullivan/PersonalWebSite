7ebac83787f2a61913a6795f0bf54443ed192f4b
patricksullivan <patdsully88@gmail.com>
2014-05-02 18:32:56 -0500

    navigation working on mobile. Still need to make smooth

--- PatrickSullivanWebSite/Scripts/site.js
+++ PatrickSullivanWebSite/Scripts/site.js
@@ -1,98 +1,114 @@
 ﻿
 $(function () {
 
-    var smallSize = 650;
+    var smallSize = 1200;
     $("[data-toggle='tooltip']").tooltip();//initializing tooltip stuff
-    ShowHideBasedOnSize();
 
+    //one page scroll initialization
     $(".main").onepage_scroll({
         sectionContainer: "section",
         afterMove: function (index) {
-                
-            $('#homeNavListItem').removeClass('active');
-            $('#aboutNavListItem').removeClass('active');
-            $('#workNavListItem').removeClass('active');
-            $('#linksNavListItem').removeClass('active');
-            $('#contactNavListItem').removeClass('active');
-
-            switch(index)
-            {
-                case 1:
-                    $('#homeNavListItem').addClass('active');
-                    break;
-                case 2:
-                    $('#aboutNavListItem').addClass('active');
-                    break;
-                case 3:
-                    $('#workNavListItem').addClass('active');
-                    break;
-                case 5:
-                    $('#linksNavListItem').addClass('active');
-                    break;
-                case 4:
-                    $('#contactNavListItem').addClass('active');
-                    break;
-            }
+            AfterOnePageScrollMove(index);
         },
         loop: true,
         responsiveFallback: smallSize
     });
 
-    var pdf = new PDFObject({
-        url: "http://www.pdsullivan.com/PatrickSullivan Resume.pdf",
-        id: "pdfRendered",
-        pdfOpenParams: {
-            view: "FitH"
-        }
-    }).embed("pdfRenderer");
+    function AfterOnePageScrollMove(index) {
+        RemoveAllActiveClassesFromNav();
 
-    //size functions
-    $(window).resize(function () {
-        ShowHideBasedOnSize();
-    });
-
-    $("#homeNavItem").click(function () {
-        $('#homeNavListItem').addClass('active');
-        $('.main').moveTo(1);
-    });
-    $("#aboutNavItem").click(function () {
-        $('#aboutNavListItem').addClass('active');
-        $(".main").moveTo(2);
-    });
-    $("#workNavItem").click(function () {
-        $('#workNavListItem').addClass('active');
-        $(".main").moveTo(3);
-    });
-    $("#linksNavItem").click(function () {
-        $('#linksNavListItem').addClass('active');
-        $(".main").moveTo(5);
-    });
-    $("#contactNavItem").click(function () {
-        $('#contactNavListItem').addClass('active');
-        $(".main").moveTo(4);
-    });
-    $("#brandNavButton").click(function () {
-        $(".main").moveTo(1);
-    });
-
-    $("#btnContactMe").click(function () {
-        $('#contactNavListItem').addClass('active');
-        $(".main").moveTo(4);
-    })
-
-    function ShowHideBasedOnSize() {
-        var width = $(window).width();
-        if (width < smallSize) {
-            $('#pdfRenderer').hide();
-            $('#toggleNavButton').hide();
-            $('#mobilePDFMessage').show();
-        }
-        else {
-            $('#pdfRenderer').show();
-            //$('#toggleNavButton').show();
-            $('#mobilePDFMessage').hide();
+        switch (index) {
+            case 1:
+                $('#homeNavListItem').addClass('active');
+                break;
+            case 2:
+                $('#aboutNavListItem').addClass('active');
+                break;
+            case 3:
+                $('#workNavListItem').addClass('active');
+                break;
+            case 5:
+                $('#linksNavListItem').addClass('active');
+                break;
+            case 4:
+                $('#contactNavListItem').addClass('active');
+                break;
         }
     };
+
+    function ScrollToSection(sectionNumber) {
+
+        RemoveAllActiveClassesFromNav();
+
+        if (LargeSize()) {
+            $('.main').moveTo(sectionNumber);
+        } else {
+            var page = 'page' + sectionNumber;
+
+            document.getElementById(page).scrollIntoView();
+
+            //attempt at offset not done yet
+            //Element.prototype.documentOffsetTop = function () {
+            //    return this.offsetTop + (this.offsetParent ? this.offsetParent.documentOffsetTop() : 0);
+            //};
+
+            //var top = document.getElementById(page).documentOffsetTop() - 45;
+            //console.log(top);
+            //window.scrollTo( 0, top );
+        }
+    };
+
+    function RemoveAllActiveClassesFromNav() {
+        $('#homeNavListItem').removeClass('active');
+        $('#aboutNavListItem').removeClass('active');
+        $('#workNavListItem').removeClass('active');
+        $('#linksNavListItem').removeClass('active');
+        $('#contactNavListItem').removeClass('active');
+    };
+
+    function LargeSize() {
+        var width = $(window).width();
+        
+        if (width > smallSize) {
+            console.log('LARGE SIZE');
+            return true;
+        } else {
+            console.log('SMALL SIZE');
+            return false;
+        }
+    };
+
+
+    //nav click functions
+    $("#homeNavItem").click(function () {
+        ScrollToSection(1);
+        $('#homeNavListItem').addClass('active');
+    });
+    $("#aboutNavItem").click(function () {
+        ScrollToSection(2);
+        $('#aboutNavListItem').addClass('active');
+    });
+    $("#workNavItem").click(function () {
+        ScrollToSection(3);
+        $('#workNavListItem').addClass('active');
+    });
+    $("#linksNavItem").click(function () {
+        ScrollToSection(5);
+        $('#linksNavListItem').addClass('active');
+    });
+    $("#contactNavItem").click(function () {
+        ScrollToSection(4);
+        $('#contactNavListItem').addClass('active');
+    });
+    $("#brandNavButton").click(function () {
+        ScrollToSection(1);
+        $('#homeNavListItem').addClass('active');
+    });
+    $("#btnContactMe").click(function () {
+        ScrollToSection(4);
+        $('#contactNavListItem').addClass('active');
+    });
+
 });
 
 
