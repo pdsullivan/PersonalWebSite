bb5b4e4faee64ed42b0e541f4a012a2b647216d9
Patrick Sullivan <patdsully88@gmail.com>
2014-08-20 12:15:57 -0500

    added source to log data in javascript

--- app/controllers/loggingController.js
+++ app/controllers/loggingController.js
@@ -5,13 +5,11 @@
         // create a blank object to hold our form information
         // $scope will allow this to pass between controller and view
         $scope.logData = {};
-        // process the form
+        // process the data
         $scope.createLogEntry = function () {
             var today = new Date();
             $scope.logData.date = today;
-            //$scope.logData.message = "";
-            //$scope.logData.details = "";
-            //$http.post('http://pdsullivan.azurewebsites.net/api/logging', JSON.stringify($scope.logData), { headers: { 'Content-Type': 'application/json; charset=utf-8' } })
+            
             $http.post('http://pdsullivan.azurewebsites.net/api/logging', JSON.stringify($scope.logData))
             .success(function (data) {
                 $scope.logData = {};
@@ -25,22 +23,23 @@
         //alert('init logging controller');
 
         $scope.init = function () {
-            var clientData = "{ ";
-            $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
-                
-                var firsttime = true;
-                $.each(data, function (k, v) {
-                    if (!firsttime) {
-                        clientData += " , "
-                    }
-                    clientData += " \"" + k + "\" : \"" + v + "\"  ";
-                    firsttime = false;
-                });
-                clientData += " }";
-            //alert(clientData);
-            $scope.logData.message = "PAGELOAD";
-            $scope.logData.details = clientData.toString();
-            $scope.createLogEntry();
+                var clientData = "{ ";
+                $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
+                    
+                    var firsttime = true;
+                    $.each(data, function (k, v) {
+                        if (!firsttime) {
+                            clientData += " , "
+                        }
+                        clientData += " \"" + k + "\" : \"" + v + "\"  ";
+                        firsttime = false;
+                    });
+                    clientData += " }";
+                //alert(clientData);
+                $scope.logData.message = "PAGELOAD";
+                $scope.logData.details = clientData.toString();
+                $scope.logData.source = document.url.toString();
+                $scope.createLogEntry();
             });
         };
 
